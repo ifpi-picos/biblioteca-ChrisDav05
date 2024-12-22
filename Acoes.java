f9ee395d697ff70bbc3d989c6c77b26d7c6b4b8f
ChrisDav05 <davioteotonio05@gmail.com>
2024-12-22 18:08:54 -0300

    Criação do método CadUsuario; criação da associação do empréstimo do usuário com o livro emprestado; criação de restrição de ano de publicação do livro.

--- Acoes.java
+++ Acoes.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,14 +14,20 @@ public class Acoes {
     ArrayList<Emprestimo> emprestimos = new ArrayList<>();
 
     public void Cadastrar(){
-          System.out.println("Digite o autor do livro:");
+          System.out.println("\nDigite o autor do livro:");
           String autor = scanner.nextLine();
           System.out.println("Digite o título do livro:");
           String titulo = scanner.nextLine(); 
           System.out.println("Digite a editora do livro:");
           String editora = scanner.nextLine();
+          int anoAtual = LocalDate.now().getYear();
+          System.out.println("Ano atual: " + anoAtual);
           System.out.println("Digite o ano do livro:");
           int ano = scanner.nextInt();
+          while (ano > anoAtual) {
+            System.out.println("O ano do livro não pode ser maior que o ano atual. Tente novamente:");
+            ano = scanner.nextInt(); 
+        }
           scanner.nextLine();
           System.out.println("Digite o ISBN do livro:");
           String ISBN = scanner.nextLine();
@@ -30,7 +37,7 @@ public class Acoes {
           livros.add(livro);
 
           System.out.println("\nLivro cadastrado na biblioteca!");
-          System.out.println("O autor do livro é: " + livro.getAutor());
+          System.out.println("\nO autor do livro é: " + livro.getAutor());
           System.out.println("O título do livro é: " + livro.getTitulo());
           System.out.println("A editora do livro é: " + livro.getEditora());
           System.out.println("O ano de publicação do livro é: " + livro.getAno());
@@ -38,21 +45,20 @@ public class Acoes {
           System.out.println("O ISBN do livro é: " + livro.getISBN());
           
     }
-    public void listarLivros(){
+    public void ListarLivros(){
         System.out.println("\nLivros cadastrados na biblioteca:");
             for (Livro livro : livros) {
-                System.out.printf("Autor: " + livro.getAutor());
-                System.out.printf("Título: " + livro.getTitulo());
-                System.out.printf("Editora: " + livro.getEditora());
-                System.out.printf("Ano: " + livro.getAno());
-                System.out.printf("Emprestado: " + livro.getEmprestado());
-                System.out.printf("ISBN: " + livro.getISBN());
-                System.out.printf("---------------------------");
+                System.out.println("\nAutor: " + livro.getAutor());
+                System.out.println("Título: " + livro.getTitulo());
+                System.out.println("Editora: " + livro.getEditora());
+                System.out.println("Ano: " + livro.getAno());
+                System.out.println("Emprestado: " + livro.getEmprestado());
+                System.out.println("ISBN: " + livro.getISBN());
+                System.out.println("---------------------------");
             }
     }
-
-    public void emprestar(){
-        System.out.println("Digite o seu nome:");
+    public void CadUsuario(){
+        System.out.println("\nDigite o seu nome:");
         String nome = scanner.nextLine();
         System.out.println("Digite o seu cpf:");
         String cpf = scanner.nextLine(); 
@@ -64,12 +70,31 @@ public class Acoes {
         usuarios.add(usuario);
         
         System.out.println("\nUsuario cadastrado na biblioteca!");
-        System.out.println("Nome: " + usuario.getNome());
+        System.out.println("\nNome: " + usuario.getNome());
         System.out.println("CPF: " + usuario.getCpf());
         System.out.println("Email: " + usuario.getEmail());
-
-        System.out.println("Digite o ISBN do livro que deseja tomar como emprestado:\n");
+    }
+    public void Emprestar(){
+        System.out.println("Lista dos livros da biblioteca");
+        ListarLivros();
+        System.out.println("\nDigite o ISBN do livro que deseja tomar como emprestado:");
         String isbn = scanner.nextLine();
+
+        System.out.println("Digite o nome do usuario que quer tomar emprestado um livro:");
+        String nomeProcura = scanner.nextLine();
+
+        Usuario usuarioEncontrado = null;
+        for (Usuario usuario : usuarios) {
+        if (nomeProcura.equals(usuario.getNome())) {
+            usuarioEncontrado = usuario;
+            break;
+        }
+    }
+        if(usuarioEncontrado!= null){
+            System.out.println("Usuario encontrado!");
+        } else {
+            System.out.println("Usuario não encontrado.");
+        }
 
         Livro livroEncontrado = null;
         for (Livro livro : livros) {
@@ -86,7 +111,7 @@ public class Acoes {
         } else {
             System.out.println("ISBN fornecido é invalido.");
         }
-        emprestimos.add(new Emprestimo(livroEncontrado, usuario));
+        emprestimos.add(new Emprestimo(livroEncontrado, usuarioEncontrado));
         for (Emprestimo emprestimo : emprestimos){
             System.out.println("Livro "+emprestimo.getLivro().getTitulo() + " está emprestado para " + emprestimo.getUsuario().getNome());
         }
--- App.java
+++ App.java
@@ -9,23 +9,24 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Livro> livrosEmprestados = new ArrayList<>();
         Acoes obj = new Acoes();
         
         while (true) {
         System.out.println("\nSEJA BEM-VINDO A BIBLIOTECA QUE TEM UM BOOKADO DE LIVRO");
-        System.out.println("\n1 - Cadastrar livro\n2 - Listar todos os livros\n3 - Pegar livro emprestado\n4 - Devolver livro\n5 - Listar livros emprestados e disponíveis\n6 - Listar histórico de empréstimos do usuário\n7 - Sair");
+        System.out.println("\n0 - Cadastrar livro\n1 - Listar todos os livros\n2 - Cadastrar usuário\n3 - Pegar livro emprestado\n4 - Devolver livro\n5 - Listar livros emprestados e disponíveis\n6 - Listar histórico de empréstimos do usuário\n7 - Sair");
         System.out.print("\nDigite uma opção:");
         int escolha = scanner.nextInt();    
         scanner.nextLine();
 
-        if (escolha == 1){ 
+        if (escolha == 0){ 
             obj.Cadastrar();
+        } else if (escolha == 1){
+            obj.ListarLivros();
         } else if (escolha == 2){
-            obj.listarLivros();
+            obj.CadUsuario();
         } else if (escolha == 3){
-            obj.emprestar();
-            }
+            obj.Emprestar();
+        }
         }
         }
 }
